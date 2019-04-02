33c2fdf6fcda6fde82d27bd69592eecf6c36f913
Matti Wada <matti.wada@gmail.com>
2019-04-02 23:47:43 +0300

    Kuvani

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Footer from './Footer'
-import matti from '../assets/images/thumbs/matti.jpg'
+import matti from '../assets/images/matti.jpg'
 
 class Header extends React.Component {
     render() {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image matti"><img src={matti} alt="" /></a>
+                    <a href="#" className="image matti"><img src={matti} alt="matti" /></a>
                     <h1><strong>I am Strata</strong>, a super simple<br />
                     responsive site template freebie<br />
                     crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
