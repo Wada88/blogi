511e0c145daa047a0f9e0688eb06798f2926e100
Matti Wada <matti.wada@gmail.com>
2019-04-02 23:12:04 +0300

    Profiilikuva

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <a href="#" className="matti"><img src={matti} alt="" /></a>
                     <h1><strong>I am Strata</strong>, a super simple<br />
                     responsive site template freebie<br />
                     crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
