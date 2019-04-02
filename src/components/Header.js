c5d9a10c90731d74e5c0076504930e8d222ca9ff
Matti Wada <matti.wada@gmail.com>
2019-04-02 23:29:04 +0300

    Profiilikuva

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/avatar.jpg'
+import avatar from '../assets/images/matti.jpg'
 
 class Header extends React.Component {
     render() {
