3368a213c7777913e80047b0e7a12ac62eee2584
Matti Wada <matti.wada@gmail.com>
2019-04-05 01:48:22 +0300

    profile and footer commits

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,29 @@ import Footer from './Footer'
 import matti from '../assets/images/matti.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image matti"><img src={matti} alt="matti"/></a>
-                    <h1><strong>Yuki Matti Wada</strong>
-                  
-                    
-                    <br></br><a href="http://html5up.net">My CV</a></h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image matti">
+            <img src={matti} alt="matti" />
+          </a>
+          <h1>
+            <strong>Yuki Matti Wada</strong>
+            <p>
+              {' '}
+              Finnish-Japanese entrepreneur
+              <br />
+              Upcoming web developer
+            </p>
+            <br />
+            <a href="http://html5up.net">My CV</a>
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
